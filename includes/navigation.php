01b4c7f3e887bbcce5a3542d2f76acc479da30ee
Dominik Modrzejewski <dominikmodrzejewski@pw.pl>
2022-04-17 10:46:10 +0200

    Creating a custom search engine, adding categories to the sidebar, admin panel

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,10 +1,9 @@
-
-
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -16,28 +15,28 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <?php
-                    global $connection;
-                    $query = "SELECT * FROM categories";
-                    $select_all_categories_query = mysqli_query($connection, $query);
+                global $connection;
+                $query = "SELECT * FROM categories";
+                $select_all_categories_query = mysqli_query($connection, $query);
 
-                    while($row = mysqli_fetch_assoc($select_all_categories_query)) {
-                        $cat_title = $row['cat_title'];
-                        echo "<li><a href='#'>{$cat_title}</a></li>";
-                    }
+                while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                    $cat_title = $row['cat_title'];
+                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                }
                 ?>
 
-<!---->
-<!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
+
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
 <!--                <li>-->
 <!--                    <a href="#">Services</a>-->
 <!--                </li>-->
 <!--                <li>-->
 <!--                    <a href="#">Contact</a>-->
 <!--                </li>-->
-<!---->
-<!--                -->
+
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
